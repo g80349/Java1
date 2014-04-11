ae0488f339d45bb05f3a8927871a1bfe4cde8052
David Whimple <davidwhimple@fullsail.edu>
2014-04-10 17:01:12 -0700

    completed weather week 2 project

--- Week2/Weather/src/com/example/weather/MainActivity.java
+++ Week2/Weather/src/com/example/weather/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.weather;
 
-
 import com.whimple.library.JSON;
 
 import android.support.v7.app.ActionBarActivity;
@@ -12,40 +11,79 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Bundle;
 
-
 public class MainActivity extends ActionBarActivity {
 
+	// variables
 	Spinner spinnerLoc;
-	String [] locList;
+	String[] locList;
+	Spinner spinnerMeasure;
+	String[] measurementList;
+	String selectedMeasurement;
 
-	 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);   
-        
-        locList = getResources().getStringArray(R.array.location_array);
-        
-        spinnerLoc = (Spinner) findViewById(R.id.location);
-        spinnerLoc.setOnItemSelectedListener(new OnItemSelectedListener(){
-        	public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
-        		Toast.makeText(spinnerLoc.getContext(), "You have selected " + locList[position], Toast.LENGTH_LONG).show();
-        	}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// get location spinner list and item on spinner that is selected
+		locList = getResources().getStringArray(R.array.location_array);
+		spinnerLoc = (Spinner) findViewById(R.id.location);
+		spinnerLoc.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				// text for toast from selected spinner item
+				Toast.makeText(spinnerLoc.getContext(),
+						"You have selected " + locList[position],
+						Toast.LENGTH_LONG).show();
+			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> arg0) {
 				// TODO Auto-generated method stub
-				
-			}
-        });
-    };
 
-    public void onClick(View view){
-    	
-    	String selectedLocation = spinnerLoc.getSelectedItem().toString();
-    	TextView results = (TextView)findViewById(R.id.forecast);
-    	results.setText(JSON.readJSON(selectedLocation));
-    }
-    
-    
+			}
+		});
+
+		// get measurement spinner list and item on spinner that is selected
+		measurementList = getResources().getStringArray(
+				R.array.measurement_array);
+		spinnerMeasure = (Spinner) findViewById(R.id.measurement);
+		spinnerMeasure.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				// text for toast from selected spinner item
+				Toast.makeText(spinnerLoc.getContext(),
+						"You have selected " + measurementList[position],
+						Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	};
+
+	public void onClick(View view) {
+
+		// get the selected location spinner and compare strings for to enum
+		// names.
+		// change name of spinner to appropriate name for enum
+		String selectedLocation = spinnerLoc.getSelectedItem().toString();
+		TextView results = (TextView) findViewById(R.id.forecast);
+		if (selectedLocation.equals("Las Vegas")) {
+			selectedLocation = "Vegas";
+		}
+		if (selectedLocation.equals("North Las Vegas")) {
+			selectedLocation = "NLV";
+		}
+		// get the selected measurement spinner name and put both spinner
+		// selections in the read JSON method
+		selectedMeasurement = spinnerMeasure.getSelectedItem().toString();
+
+		results.setText(JSON.readJSON(selectedLocation, selectedMeasurement));
+
+	}
+
 }
--- Week2/Weather/src/com/whimple/library/JSON.java
+++ Week2/Weather/src/com/whimple/library/JSON.java
@@ -5,64 +5,83 @@ import org.json.JSONObject;
 
 public class JSON {
 
-	public static JSONObject buildJSON(){
-		
+	// builds JSON from locations.java enum values
+	public static JSONObject buildJSON() {
+
 		JSONObject locationObject = new JSONObject();
-		
+
 		try {
 			JSONObject inquiryObject = new JSONObject();
-			
-			for (Locations location : Locations.values()){
-				
+
+			for (Locations location : Locations.values()) {
+
 				JSONObject locObject = new JSONObject();
-				
+
 				locObject.put("temp", location.setTemp());
 				locObject.put("lowTemp", location.lowTemp());
 				locObject.put("sky", location.sky());
 				locObject.put("humidity", location.humidity());
 				locObject.put("windSpeed", location.windSpeed());
+				locObject.put("cTemp", location.cTemp());
+				locObject.put("cLowTemp", location.cLowTemp());
 				inquiryObject.put(location.name().toString(), locObject);
 			}
-			
-			locationObject.put("inquiry", inquiryObject);	
-			
+
+			locationObject.put("inquiry", inquiryObject);
+
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return locationObject;
 	}
-	
-	public static String readJSON (String selectedLocation){
-		
-		String output = null, temp, lowTemp, sky, humidity, windSpeed;
+
+	// read JSON
+	public static String readJSON(String selectedLocation,
+			String selectedMeasurement) {
+
+		// variables for the values from the enums after build JSON
+		String output = null, temp, lowTemp, sky, humidity, windSpeed, cTemp, cLowTemp;
+
 		JSONObject jsonObject = buildJSON();
-		
+
 		try {
-			temp = jsonObject.getJSONObject("inquiry").getJSONObject(selectedLocation).getString("temp");
-			lowTemp = jsonObject.getJSONObject("inquiry").getJSONObject(selectedLocation).getString("lowTemp");
-			sky = jsonObject.getJSONObject("inquiry").getJSONObject(selectedLocation).getString("sky");
-			humidity = jsonObject.getJSONObject("inquiry").getJSONObject(selectedLocation).getString("humidity");
-			windSpeed = jsonObject.getJSONObject("inquiry").getJSONObject(selectedLocation).getString("windSpeed");
-			
-			output = "Temperature: " + temp  + "deg F"
-					+ "\r\n"
-					+ "Low: " + lowTemp + "deg F"
-					+ "\r\n"
-					+ "Humidity: " + humidity + "%"
-					+ "\r\n"
-					+ sky  
-					+ "\r\n"
-					+ windSpeed
-					+ "\r\n";
-					
-			
+			temp = jsonObject.getJSONObject("inquiry")
+					.getJSONObject(selectedLocation).getString("temp");
+			lowTemp = jsonObject.getJSONObject("inquiry")
+					.getJSONObject(selectedLocation).getString("lowTemp");
+			sky = jsonObject.getJSONObject("inquiry")
+					.getJSONObject(selectedLocation).getString("sky");
+			humidity = jsonObject.getJSONObject("inquiry")
+					.getJSONObject(selectedLocation).getString("humidity");
+			windSpeed = jsonObject.getJSONObject("inquiry")
+					.getJSONObject(selectedLocation).getString("windSpeed");
+			cTemp = jsonObject.getJSONObject("inquiry")
+					.getJSONObject(selectedLocation).getString("cTemp");
+			cLowTemp = jsonObject.getJSONObject("inquiry")
+					.getJSONObject(selectedLocation).getString("cLowTemp");
+
+			// if spinner is fahrenheit
+			if (selectedMeasurement.equals("Fahrenheit")) {
+				output = "Temperature: " + temp + "deg F" + "\r\n" + "Low: "
+						+ lowTemp + "deg F" + "\r\n" + "Humidity: " + humidity
+						+ "%" + "\r\n" + sky + "\r\n" + windSpeed + "\r\n";
+			}
+			// if spinner is celcius
+			if (selectedMeasurement.equals("Celcius")) {
+
+				output = "Temperature: " + cTemp + "deg C" + "\r\n" + "Low: "
+						+ cLowTemp + "deg C" + "\r\n" + "Humidity: " + humidity
+						+ "%" + "\r\n" + sky + "\r\n" + windSpeed + "\r\n";
+
+			}
+
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return output;
 	}
 
